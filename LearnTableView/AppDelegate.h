36fd33cd8adc2b85ccba5ac2dcd5a63bad483b33
hakerfreedom <freedamhaker@gmail.com>
2016-07-25 17:45:12 +0900

    Initial Commit

---
+++ LearnTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearnTableView
+//
+//  Created by DDaima on 7/25/16.
+//  Copyright © 2016 DDaima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LearnTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LearnTableView
+//
+//  Created by DDaima on 7/25/16.
+//  Copyright © 2016 DDaima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
